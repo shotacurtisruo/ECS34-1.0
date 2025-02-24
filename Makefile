b75967c559f13f1266d77fb21fb6f4a2e8f096c3
Shota Ruo <159093194+shotacurtisruo@users.noreply.github.com>
2025-02-23 18:12:08 -0800

    newline add

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 CXX = g++
-CXXFLAGS = -std=c++17 -Wall -g -Iinclude -I/opt/homebrew/include
+CXXFLAGS = -std=c++17 -Wall -g -Iproj1/include -I/opt/homebrew/include
 LDFLAGS = -L/opt/homebrew/lib -lgtest -lgtest_main -lpthread
 OBJ_DIR = obj
 BIN_DIR = bin
 
 
 # Source files (update if filenames differ)
-SRC = src/StringUtils.cpp
-TEST_SRC = testsrc/StringUtilsTest.cpp
+SRC = proj1/src/StringUtils.cpp
+TEST_SRC = proj1/testsrc/StringUtilsTest.cpp
 
 
 # Object files (must match source filenames)
@@ -31,13 +31,13 @@ $(TARGET): $(OBJS)
 
 
 # Compile source files
-$(OBJ_DIR)/%.o: src/%.cpp
+$(OBJ_DIR)/%.o: proj1/src/%.cpp
 	@echo "didit compile the source files -"
 	@$(CXX) $(CXXFLAGS) -c $< -o $@
 
 
 # Compile test files
-$(OBJ_DIR)/%.o: testsrc/%.cpp
+$(OBJ_DIR)/%.o: proj1/testsrc/%.cpp
 	@echo "did it compile the test files -"
 	@$(CXX) $(CXXFLAGS) -c $< -o $@
 
@@ -52,5 +52,3 @@ clean:
 	@echo "did this work - the cleaning part"
   #these are just to see which ones ran and worked.
 	@rm -rf $(OBJ_DIR) $(BIN_DIR)
-
-
--- proj1/src/StringUtils.cpp
+++ proj1/src/StringUtils.cpp
@@ -168,24 +168,50 @@ namespace StringUtils
         }
         return result;
     }
-
     std::vector<std::string> Split(const std::string &str, const std::string &splt) noexcept
     {
-        // Replace code her
-        // this splits the strin gup into a vector of strings based on splt parameter.
-        // if splt parameter is an empty string, split on white space
+        std::vector<std::string> v; // Vector store stringds
+        std::string res = str;      // Make a copy of the input string to edit
 
-        std::vector<std::string> v;
-        std::string res = str;
-        auto ind = str.find(splt);
-        while (ind != std::string::npos)
+        //if the delimiter is empty, split by whitespace
+        if (splt == "")
         {
-            v.push_back(res.substr(0, ind));
-            res = res.substr(ind + 1);
-            ind = res.find(splt);
+            size_t start = 0; 
+            for (size_t i = 0; i < res.length(); i++)
+            {
+         
+                if (res[i] == ' '||res[i]=='\n')//|| res[i] == '\t' || res[i] == '\n tabs or new line i dnt think i need unless later 
+                {
+                    if (i > start)
+                    {
+                        v.push_back(res.substr(start, i - start)); // Add the word before the whitespace
+                    }
+                    start = i + 1;
+                }
+            }
+            // Add the last part 
+            if (start < res.length())
+            {
+                v.push_back(res.substr(start));
+            }
         }
-        v.push_back(res);
-        return v;
+        else // Case 2: If a specific delimiter is given, split by the delimiter
+        {
+            size_t start = 0;
+            size_t ind = res.find(splt); // Find the first occurrence of the delimiter
+
+            while (ind != std::string::npos) // Loop while we keep finding the delimiter
+            {
+                v.push_back(res.substr(start, ind - start)); // Extract substring before delimiter and store
+                start = ind + splt.length();                 // Move past the delimiter
+                ind = res.find(splt, start);                 // Find the next occurrence of the delimiter
+            }
+
+            // Add the last part of the string after the last delimiter
+            v.push_back(res.substr(start));
+        }
+
+        return v; // Return the vector containing the split parts
     }
 
     std::string Join(const std::string &str, const std::vector<std::string> &vect) noexcept
@@ -201,28 +227,96 @@ namespace StringUtils
     std::string ExpandTabs(const std::string &str, int tabsize) noexcept
     {
 
-        std::string result;
-
-        for (size_t i = 0; i < str.length(); i++)
+        std::string res;
+        size_t curr = 0;
+        if (tabsize == 0)
         {
-            if (str[i] == '\t')
+            for (char c : str)
             {
-                int temp = tabsize - (result.length() % tabsize);
-                result.append(temp, ' ');
+                if (c != '\t')
+                {
+                    res += c;
+                }
+            }
+            return res;
+        }
+        for (char c : str)
+        {
+            if (c == '\t')
+            {
+                size_t numspaces = tabsize - (curr % tabsize);
+                res.append(numspaces, ' ');
+                curr += numspaces;
             }
             else
             {
-
-                result += str[i];
+                res += c;
+                curr += 1;
             }
         }
-
-        return result;
+        return res;
     }
 
     int EditDistance(const std::string &left, const std::string &right, bool ignorecase) noexcept
     {
         // Replace code here
-        return 0;
+        std::string leftTemp = left;
+        std::string rightTemp = right;
+        // must be lowercase if ignorecase is true
+        if (ignorecase)
+        {
+            leftTemp = StringUtils::Lower(left);
+            rightTemp = StringUtils::Lower(right);
+        }
+        // now get the length actual sizes of the strings
+        size_t leftSize = leftTemp.size();
+        size_t rightSize = rightTemp.size();
+        // create 2D vector to hold the distances of the strings
+        std::vector<std::vector<int>> distance(leftSize + 1, std::vector<int>(rightSize + 1));
+        // now this should look like a table
+
+        for (size_t i = 0; i <= leftSize; i++)
+        {
+            distance[i][0] = i;
+        }
+        // this is for the inital row
+        for (size_t j = 0; j <= rightSize; j++)
+        {
+            distance[0][j] = j;
+        }
+        // fill rest of the table
+        for (size_t i = 1; i <= leftSize; i++)
+        {
+            for (size_t j = 1; j <= rightSize; j++)
+            {
+                int temp = 1;
+                if (leftTemp[i - 1] == rightTemp[j - 1])
+                {
+                    temp = 0;
+                }
+
+                int insertion = distance[i][j - 1] + 1;
+                int deletion = distance[i - 1][j] + 1;
+                int substitution = distance[i - 1][j - 1] + temp;
+                // these are all the costs of the operations
+
+                if (insertion <= deletion && insertion <= substitution)
+                {
+                    distance[i][j] = insertion;
+                }
+                else if (deletion <= insertion && deletion <= substitution)
+                {
+                    distance[i][j] = deletion;
+                }
+                else
+                {
+                    distance[i][j] = substitution;
+                }
+                // this is the minimum of the three
+            }
+        }
+        // now we have the table filled out, so we can return the last element
+        // this is the distance between the two strings
+        return distance[leftSize][rightSize];
     }
-};
+}
